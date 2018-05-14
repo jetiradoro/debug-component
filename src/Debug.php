f2f2a036d9d489e4566cde45d2d564e085316663
Jesús.T <jtirado@tecnocampus.cat>
2018-05-14 11:03:30 +0200

    set component

---
+++ src/Debug.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tecnocampus\Debug;
+
+class Debug {
+	public static function s($array, $finish = false) {
+		print "<pre style='background: #eee; padding: 20px; color:black;'>";
+		print_r ($array);
+		print "</pre>";
+		if ($finish) {
+			die();
+		}
+	}
+	public static function x($array) {
+		self:s($array, true);
+	}
+}
